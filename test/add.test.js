49f7b2c94a616ed3cb07c8ebdf549c2587c0ce5a
pengyong <pengyong@zhixuezhen.com>
2018-08-13 17:37:17 +0800

    test: single run: true

--- test/add.test.js
+++ test/add.test.js
@@ -1,7 +1,9 @@
 var assert = chai.assert;
 
-describe("加法函数的测试", function() {
-  it("1 加 1 应该等于 2", function() {
-    assert.equal(jtools.add(1, 1), 2);
+describe("test", function() {
+  describe("加法函数的测试", function() {
+    it("1 加 1 应该等于 2", function() {
+      assert.equal(jtools.add(1, 1), 2);
+    });
   });
 });
